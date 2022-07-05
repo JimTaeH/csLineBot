b54abb1ec3cd680c6ec5fab3b7e5799fcb9d408c
Sahaz <60215727+JimTaeH@users.noreply.github.com>
2022-07-05 09:48:34 +0700

    Update Datacontext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -32,7 +32,7 @@ namespace MyLineBot.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=27.254.207.172;Database=00008;user id=sa;password=skyfr0gDEV;Trusted_Connection=False;");
+                optionsBuilder.UseSqlServer("Connection String");
             }
         }
 
